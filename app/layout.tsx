2d00c1df360ef5eebd1f7a9b0862e083d4d67e3b
henaorth16 <emyayehenok@gmail.com>
2026-04-20 00:50:16 +0300

    chore: add web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Analytics />
         <Footer name="DAWIT" year={2024} />
       </body>
     </html>
